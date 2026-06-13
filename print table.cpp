c8f7518748ba0f53c64f7db170031485cef08243
Muhammad Talha <talhashkeel1@gmail.com>
2026-06-13 23:30:11 +0500

    Add files via upload

---
+++ print table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ; 
+int main() {
+	int num;
+	cout<< "enter the num"; 
+	cin>>num;
+	for( int i=1;i<=10;i++){
+	 int  table = num*i;
+		cout<< table;
+		cout<< endl;
+	}
+	
+	
+	return 0;
+}
